2bd6690e4f21f67726c26ba4770bf62f90f74cc7
Bill Mill <bill.mill@gmail.com>
2014-02-23 12:34:49 -0500

    first version that works

--- backup.py
+++ backup.py
@@ -1,20 +1,3 @@
-#import praw
-#import json
-#from pprint import pformat as pf
-#
-#r = praw.Reddit(user_agent='backupcomments')
-#r.config.store_json_result = True
-#me = r.get_redditor("llimllib")
-#comments = me.get_comments(count=25)
-#fout = open("comments.json", 'w')
-#fout.write("[\n")
-#for comment in comments:
-#    print comment
-#    fout.write(json.dumps(comment.json_dict, indent=4))
-#    fout.write(",\n")
-#fout.write('"null"]')
-#fout.close()
-
 import json
 import requests
 import time
@@ -25,26 +8,27 @@ after = ""
 n = 25
 count = n
 
-fout = open("comments.json", 'w')
 data = []
 
-while 1:
-    url = urltemplate.format(count, after)
-    print url
-    r = requests.get(url, headers=headers).json()
+try:
+    while 1:
+        url = urltemplate.format(count, after)
+        print url
+        r = requests.get(url, headers=headers).json()
 
-    fout.write(json.dumps(r.json(), indent=4))
-    fout.write(",")
+        things = r["data"]["children"]
+        data += things
 
-    last_id = r.json()["data"]["children"][-1]["data"]["name"]
-    if last_id == after:
-        from pprint import pprint as pp
-        pp(r.json()["data"]["children"][-1])
-        print last_id, after
-        break
-    after = last_id
-    count += n
-    time.sleep(2)
+        last_id = r["data"]["after"]
+        if not last_id or last_id == after:
+            break
 
-fout.write('""]')
+        after = last_id
+        count += n
+        time.sleep(2)
+except Exception as e:
+    print e
+
+fout = open("comments.json", 'w')
+fout.write(json.dumps(data, indent=4))
 fout.close()
